69178cef400360a9a3f54b4aa676742990f42ef8
amani esseili <esseiliamani@hotmail.com>
2023-08-27 00:06:45 +0200

    option  popolate dinamicamente commentati

--- src/components/AdvancedFilters.vue
+++ src/components/AdvancedFilters.vue
@@ -8,6 +8,35 @@ export default{
 
 
 };
+//-------------------------------------------------
+/**import axios from 'axios'
+export default{
+  data() {
+    return{
+      selectedArchetype: "",
+      archetypes: [],
+(devo aggiungere anche l'array del cards??)
+    };
+  },
+  methods:{
+    fetchArchetypes () {
+      const url = https://db.ygoprodeck.com/api/v7/archetypes.php
+
+      axios.get(url).then((response) =>{
+        this.archetypes = response.data.data;
+      });
+    },
+  },
+
+  mounted() {
+    this.fetchArchetypes();
+  },
+  
+}
+
+*/
+//---------------------------------------------------------------------
+
 
 </script>
 
@@ -27,13 +56,42 @@ export default{
             <option value="Ally of Justice">"ANoble Knight"</option>
             <option value="Ally of Justice">"Melodious"</option>
             <option value="Ally of Justice">"Tainted Treasure"</option>
-            <option value="Archfiend">Archfiend</option>
+            <option value="Archfiend">Archfiend</option> 
           </select>
         </div>
       </div>
     </div>
     
   </form>
+<!-- -------------------------------------------------------- -->
+<!-- <form action="" class="">
+    
+
+  <div class="row row-cols-2">
+    <div class="col">
+      <div class="mb-3">
+
+        <label class="form-label"></label>
+       
+        <select class="form-select" v-model="selectedArchetype">
+          
+          <option value=""></option>
+
+          <option 
+            v-for="archetype in archetypes" 
+            value="archetype_name"
+            :key="archetype.id">{{archetype.archetype_name}}
+          </option>
+
+        </select>
+      </div>
+    </div>
+  </div>
+  
+</form> -->
+
+
+
 
 </template>
 
