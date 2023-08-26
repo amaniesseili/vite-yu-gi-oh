975595288701b87f3aa19b5497fa5eaf87ee554b
amani esseili <esseiliamani@hotmail.com>
2023-08-26 20:09:03 +0200

    store.js

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,21 +13,25 @@ export default{
 
 <template>
 
-        <div class="card h-100"> 
-        <!--   8- recupero l'immagine nel :src  il nome e l'archetype-->
-          <img :src="card.image_url" class="card-img-top" alt="..."> 
-        <div class="card-body">
-          
-          <h3 class="card-title">{{card.name}}</h3>
-          <h4 class="card-title">{{card.archetype}}</h4>
-            
-            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-        </div>
+
+        <div class="card h-100 "> 
+          <!--   8- recupero l'immagine nel :src  il nome e l'archetype-->
+          <img :src="card.card_images[0].image_url_small" class="card-img-top" alt="..."> 
+        <!-- /////// perche nn funziona il d-flex neanche in css ?? ////// -->
+          <div class="card-body d-flex flex-column ">  
+            <h3 class="card-title">{{card.name}}</h3>
+            <p class="card-text">{{card.archetype}}</p>
+          </div>
       </div>
 
 </template>
 
 
 <style lang="scss" scoped>
+.card-body{
+  display: flex;
+  flex-direction: column;
+}
+
 
 </style>
